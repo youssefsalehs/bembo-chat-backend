c1665495ea41aa0601e46722132d80e5cccefcf9
youssefsalehs <youssef.ashraf.saleh@gmail.com>
2026-02-24 19:42:34 +0200

    fix: update CORS configuration to allow multiple origins

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://bembo-chat-backend-production.up.railway.app",
+    ],
     credentials: true,
   }),
 );
